ad7bf7c9496aaf703bd109e8a424071a9c3bcbc5
DuongAmin <vanduonghcmus@example.com>
2020-04-05 20:10:55 +0700

    Create application layer structure

---
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,13 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IPublicProductService // Chứa các phương thức bên ngoài 
+    {
+        PagedViewModel<ProductViewModel> GetAllByCategoryId(int categoryId,int pageIndex, int pageSize);
+    }
+}
---
+++ eShopSolution.Application/Dtos/PagedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Dtos
+{
+    public class PagedViewModel<T>
+    {
+        // Có thể dùng Page này cho tất cả đối tượng khác nhau
+        public List<T> Items { get; set; }// generic Item có tên là T
+        public int TotalRecord { get; set; }
+
+    }
+}
